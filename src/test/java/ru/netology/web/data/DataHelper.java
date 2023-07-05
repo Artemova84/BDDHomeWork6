e03fa1abc7b966b7eaef3c4daaa32fd0e0957b8f
Artemova <artemovaok84@mail.ru>
2023-07-05 14:32:19 +0300

    HomeWork with edits

--- src/test/java/ru/netology/web/data/DataHelper.java
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -1,4 +1,4 @@
-package ru.netology.web.page;
+package ru.netology.web.data;
 
 import lombok.Value;
 
@@ -7,9 +7,11 @@ import java.util.Random;
 public class DataHelper {
     private DataHelper() {
     }
+
     public static VerificationCode getVerificationCode() {
         return new VerificationCode("12345");
     }
+
     public static AuthInfo getAuthInfo() {
         return new AuthInfo("vasya", "qwerty123");
     }
@@ -21,21 +23,26 @@ public class DataHelper {
     public static CardInfo getSecondCardInfo() {
         return new CardInfo("5559 0000 0000 0002", "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
     }
+
     public static int generateValidAmount(int balance) {
         return new Random().nextInt(balance) + 1;
     }
+
     public static int generateInvalidAmount(int balance) {
         return Math.abs(balance) + new Random().nextInt(10000);
     }
+
     @Value
     public static class VerificationCode {
         String code;
     }
+
     @Value
     public static class CardInfo {
         String cardNumber;
         String testId;
     }
+
     @Value
     public static class AuthInfo {
         String login;
--- src/test/java/ru/netology/web/page/DashBoardPage.java
+++ src/test/java/ru/netology/web/page/DashBoardPage.java
@@ -3,6 +3,7 @@ package ru.netology.web.page;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+import ru.netology.web.data.DataHelper;
 
 import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Condition.visible;
